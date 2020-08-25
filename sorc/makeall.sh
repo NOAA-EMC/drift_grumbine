e611a31a6b93578845a623c32c56f25e9182b466
Robert Grumbine <grumbinespam@aol.com>
2020-08-25 10:55:51 -0400

    build @home too

--- sorc/makeall.sh
+++ sorc/makeall.sh
@@ -6,10 +6,10 @@ sys=`hostname`
 module list > /dev/null 2> /dev/null
 if [ $? -ne 0 ] ; then
   echo On a system without the module software
-  export BASE=${BASE:-/Users/rmg3/usrlocal/mmab}
+  export BASE=${BASE:-/Users/rmg3/usrlocal/mmablib}
   export FC=gfortran
   export FOPTS='-O2 -std=f2008'
-  export LIBS='-L/Volumes/ncep/nceplibs/ -L $(BASE)/$(mmablib_ver)/'
+  export LIBS='-L/Volumes/ncep/nceplibs/ -l w3emc_4 -l w3nco_4 -l bacio_4 -L $(BASE)/$(mmablib_ver)/'
   export MMAB_VER=v3.5.0
 else
 #on a system with module software, such as wcoss
@@ -20,14 +20,13 @@ else
   module load bufr/11.2.0 bacio/2.0.2
   echo in makeall.sh loaded modules:
   module list
+  export FC=ifort
+  export FOPTS='-O2 -std90'
+  export LIBS='-L $(BASE)/$(mmablib_ver)/ $(W3NCO_LIB4) $(W3EMC_LIB4) $(BACIO_LIB4)'
 fi
 
-#theia: export BASE=${BASE:-/home/Robert.Grumbine/save}
+#theia/hera: export BASE=${BASE:-/home/Robert.Grumbine/save}
 export BASE=${BASE:-/u/Robert.Grumbine/para/mmablib}
-
-export FC=ifort
-export FOPTS='-O2 -std90'
-export LIBS='-L $(BASE)/$(mmablib_ver)/ $(W3NCO_LIB4) $(W3EMC_LIB4) $(BACIO_LIB4)'
 
 #Common to all systems:
 export mmablib_ver=${mmablib_ver:-v3.5.0}
@@ -51,4 +50,7 @@ do
   cd ..
 done
 
+if [ ! -d ../exec ] ; then
+  mkdir ../exec
+fi
 mv */seaice_* ../exec 
