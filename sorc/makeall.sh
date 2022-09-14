3cf4f5cf2603912e3027837b87e6d9f77487dc6e
Robert Grumbine <Robert.Grumbine@noaa.gov>
2022-09-14 19:42:56 +0000

    remove conflict legacy from exseaice and update makeall for mmablib submodule

--- sorc/makeall.sh
+++ sorc/makeall.sh
@@ -16,33 +16,22 @@ if [ $? -ne 0 ] ; then
   export MMAB_VER=v3.5.0
 else
 #on a system with module software, such as wcoss
-#phase3   module purge
-#phase3   #Phase3
-#phase3   module load EnvVars/1.0.2 ips/18.0.1.163
-#phase3   module load w3nco/2.0.6 impi/18.0.1 w3emc/2.3.0
-#phase3   module load bufr/11.2.0 bacio/2.0.2
-#phase3   echo in makeall.sh loaded modules:
-#acorn   module list
-  #source /apps/prod/lmodules/startLmod
+#WCOSS2   module list
   module reset
-  #module avail 2> avail.1
   module load PrgEnv-intel/$PrgEnv_intel_ver
   module load intel/$intel_ver
   module load craype/$craype_ver
   module load w3nco/$w3nco_ver
   module list
-  #module avail 2> avail.2
 fi
 
 git clone https://github.com/rgrumbine/mmablib.git mmablib
 cd mmablib
-git checkout acorn
+git checkout operations
 make
 cd ..
 
-#theia: export BASE=${BASE:-/home/Robert.Grumbine/save}
-#export BASE=${BASE:-/u/Robert.Grumbine/save/mmablib}
-
+#The ftn, CC aliases require craype
 export FC=ftn
 export FOPTS='-O2 '
 
@@ -66,7 +55,7 @@ export JORG=NP21
 export JPROG=GRUMBINE
 
 #Building:
-for d in seaice_preaverage.Cd seaice_sicedrft.fd seaice_ensblend.Cd seaice_reformat.fd
+for d in seaice_preaverage.Cd seaice_sicedrft.fd seaice_midpoints.Cd seaice_reformat.fd
 do
   cd $d 
   make -i
