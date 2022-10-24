03f6fcf0e669334f4284fdb79b0354f512f822b6
Robert Grumbine <Robert.Grumbine@noaa.gov>
2022-10-24 19:19:04 +0000

    developer runs in queue

--- sms/runup.sh
+++ sms/runup.sh
@@ -1,17 +1,43 @@
 #!/bin/sh --login
-
+#PBS -N atest
+#PBS -o atest
+#PBS -j oe
+#PBS -A ICE-DEV
+#PBS -q dev
+#PBS -l walltime=0:21:00
+#PBS -l select=1:ncpus=1
+#
 #set -xe
 set -e
 
+export cyc=${cyc:-00}
+export envir=developer
+export seaice_drift_ver=v4.1.4
+export job=seaice_drift
+
+export HOMEbase=$HOME/rgdev/seaice_drift.$seaice_drift_ver/
+export SMSBIN=$HOMEbase
+cd $HOMEbase/sms/
+
+#export HOMEbase=$HOME/rgdev/slush.drift/feature/
 # WCOSS2
+
+. ../versions/seaice_drift.ver
+. ../versions/run.ver
+
 module reset
 module load envvar/1.0
-module load intel/19.1.3.304
-module load craype//2.7.8
-module load cray-mpich/8.1.7
-module load prod_envir/2.0.5
-module load prod_util/2.0.8
-module load wgrib2/2.0.8
+module load prod_util/${prod_util_ver}
+module load intel/${intel_ver}
+module load wgrib2/${wgrib2_ver}
+
+#module load craype/2.7.8
+#module load cray-mpich/8.1.7
+#module load prod_envir/2.0.5
+
+# temporary:
+#export SENDDBN=NO
+#export SENDCANNEDDBN=YES
 
 # -- to check on a module's usage: module spider $m 
 # Show what happened:
@@ -19,34 +45,18 @@ module list
 
 set -xe
 
-. ../versions/seaice_drift.ver
-. ../versions/run.ver
-
-export cyc=${cyc:-00}
-export envir=developer
-export code_ver=v4.1.4
-export job=seaice_drift
-export SMSBIN=$HOMEbase
-
-export HOMEbase=$HOME/rgdev/seaice_drift.$seaice_drift_ver/
-#export HOMEbase=$HOME/rgdev/slush.drift/feature/
-
-cd $HOMEbase/sms/
-
-tagm=20221014
-tag=20221015
+tagm=20221022
+tag=20221023
 end=`date +"%Y%m%d" `
-end=$tag
+#end=$tag
 while [ $tag -le $end ]
 do
   export PDY=$tag
   export PDYm1=$tagm
 
-  if [ ! -d $HOME/noscrub/com/mmab/developer/seaice_drift.$tag$cyc ] ; then
     export KEEPDATA="NO"         #Normal runs
-    export KEEPDATA="YES"        #debugging
+    #export KEEPDATA="YES"        #debugging
     time $HOMEbase/jobs/JSEAICE_DRIFT  > sms.${tag}${cyc}
-  fi
 
   tagm=$tag
   tag=`expr $tag + 1`
